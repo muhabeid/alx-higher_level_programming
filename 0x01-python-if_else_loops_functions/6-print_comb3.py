bfc38fd1419e187b160fef0ad9746a69cb951e46
muhabeid <muhabeid@gmail.com>
2022-06-01 02:28:21 +0300

    Changes to task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for num in range(0, 90):
-    if num % 10 > num / 10:
-        if num != 89:
-            print("{:02d}, ".format(num), end='')
+for i in range(0, 9):
+    for j in range(i + 1, 10):
+        if i == 8:
+            print("{}{}".format(i, j))
         else:
-            print("{:02d}".format(num))
+            print("{}{}".format(i, j), end=", ")
