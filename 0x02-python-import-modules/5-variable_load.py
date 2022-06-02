2fe5931a0d7a7553b865a06c6414313ec04d62b0
Mohamed A Abeid <100140806+muhabeid@users.noreply.github.com>
2022-06-02 22:40:23 +0300

    Variable load

--- 0x02-python-import-modules/5-variable_load.py
+++ 0x02-python-import-modules/5-variable_load.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    from variable_load_5 import a
-    print("{}".format(a))
+#!/usr/bin/python3 
+ from variable_load_5 import a 
+ if __name__ == "__main__": 
+     print("{:d}".format(a))
