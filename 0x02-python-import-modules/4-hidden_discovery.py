a90dc411d6c08bb04236a3a5a6e78915996567b1
Mohamed A Abeid <100140806+muhabeid@users.noreply.github.com>
2022-06-02 22:39:47 +0300

    Hidden discovery

--- 0x02-python-import-modules/4-hidden_discovery.py
+++ 0x02-python-import-modules/4-hidden_discovery.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    from hidden_4 import *
-    arr = dir()
-    for i in range(0, len(arr)):
-        if arr[i][0:2] != "__":
-            print("{}".format(arr[i]))
+import hidden_4 
+ if __name__ == "__main__": 
+     for i in dir(hidden_4): 
+         if i[:2] != "__": 
+             print(i)
